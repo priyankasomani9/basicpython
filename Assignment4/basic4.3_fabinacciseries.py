148cd7a4dbcde07891d420bbd09b7b2581a9b4da
ADMIN <somanip09@gmail.com>
2021-05-04 23:32:35 +0530

    Armstrong number finded

---
+++ Assignment4/basic4.3_fabinacciseries.py
@@ -0,0 +1,14 @@
+number=int(input("Enter any number="))
+number1=0
+number2=1
+count=0
+sum=0
+while count<number:
+    count+=1
+    print(sum)
+    sum=number1+number2
+    number1=number2
+    number2=sum
+
+
+
