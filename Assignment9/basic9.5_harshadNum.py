d5b3a123a35d7eb6c3b81078d9ee507fcf7f8867
priyanka-ai <psomani893@gmail.com>
2021-05-21 19:46:13 +0530

    check harshad number

---
+++ Assignment9/basic9.5_harshadNum.py
@@ -0,0 +1,16 @@
+number=int(input("Enter any number"))
+rem = sum = 0;
+n = number;
+while(number > 0):
+    rem = number%10;
+    sum = sum + rem;
+    number = number//10;
+
+if(n%sum == 0):
+    print(str(n) + " is a harshad number");
+else:
+    print(str(n) + " is a not harshad number");
+
+
+
+
