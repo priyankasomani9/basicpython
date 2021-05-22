b078d42f5d00d4e959be5a19a3281bf01b922b2a
priyanka-ai <psomani893@gmail.com>
2021-05-22 21:06:39 +0530

    find greatest element from list

---
+++ Assignment10/basic10.4_gretestNum.py
@@ -0,0 +1,9 @@
+list1=[]
+n=int(input("how many number of element you want in list"))
+for i in range(0,n):
+    n= int(input())
+    list1.append(n)
+print(list1)
+list1.sort()
+print("Smallest element is:", list1[-1])
+print("Smallest element is:", max(list1))
